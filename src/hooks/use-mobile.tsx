71c5cca6e77bfd190d30cf658763556334106331
Ammar Mansa <potatomasta501@gmail.com>
2025-07-22 19:41:00 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -11,9 +11,11 @@ export function useIsMobile() {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    // Set the initial value
+    onChange();
+    
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
